807f8cd8624aa4a6a9d320e5dd0d6823a03a8fe8
dangxiaoyin <dangxy@dangxy.local>
2015-12-23 20:25:38 +0800

    UserContactsInfo

--- 获取联系人信息/ViewController.m
+++ 获取联系人信息/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import <UIKit/UIKit.h>
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+@interface ViewController ()<ABPeoplePickerNavigationControllerDelegate>
+- (IBAction)Take:(id)sender;
 
 @end
 
@@ -24,4 +27,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)Take:(id)sender {
+    ABPeoplePickerNavigationController *picker = [[ABPeoplePickerNavigationController alloc] init];
+    picker.peoplePickerDelegate = self;
+    // Display only a person's phone, email, and birthdate
+    NSArray *displayedItems = [NSArray arrayWithObjects:[NSNumber numberWithInt:kABPersonPhoneProperty],
+                               [NSNumber numberWithInt:kABPersonEmailProperty],
+                               [NSNumber numberWithInt:kABPersonBirthdayProperty], nil];
+    
+    
+    picker.displayedProperties = displayedItems;
+    // Show the picker
+    
+    [self presentViewController:picker animated:YES completion:^{
+        
+    }];
+    
+}
 @end
